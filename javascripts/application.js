1e5734b61311fda13f8133a4fd3fa2b314ec2e9a
Patrick Williams <patrick@bittorrent.com>
2012-07-20 14:42:30 -0700

    adding syntax blocks

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,22 @@
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 jQuery(function() {
     $('form').submit(function(event) {
         event.preventDefault();
@@ -8,7 +27,7 @@ jQuery(function() {
             plugin: false
         });
         btapp.on('all', function() {
-            $('.container').append('<p>' + JSON.stringify(arguments) + '</p>');
+            $('.container').append('<pre>' + syntaxHighlight(JSON.stringify(arguments, undefined, 4)) + '</pre>');
         });
     });
 });
